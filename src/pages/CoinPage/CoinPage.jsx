5a9436f32d3fc7871637f6f34210100a2a096bf5
marshitsky <marshitsky@gmail.com>
2023-05-29 13:07:51 +0300

    feat(pages): add pages

---
+++ src/pages/CoinPage/CoinPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export const CoinPage = () => {
+  return <div>CoinPage</div>;
+};
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CurrencyList, NavBar, Footer } from "../../components";
+
+export const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+      <CurrencyList />
+      <Footer />
+    </>
+  );
+};
